494f90690a1a75edc361f449c2fbe70c69502052
Rita-Ari <167446189+Rita-Ari@users.noreply.github.com>
2024-05-21 13:31:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("Welcom to Password Generator App");
+
+const PasswordBox = document.getElementById("Password");
+const lenght = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwx";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+|}{[<>/-=";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function creatPassword(){
+    let Password = " ";
+    Password += upperCase[Math.floor(Math.random()* upperCase.length)];
+    Password += lowerCase[Math.floor(Math.random()* lowerCase.length)];
+    Password += number[Math.floor(Math.random()* number.length)];
+    Password += symbol[Math.floor(Math.random()* symbol.length)];
+
+    while(lenght > Password.length){
+        Password += allChars[Math.floor(Math.random()* allChars.length)];
+    }
+
+    PasswordBox.value = Password;
+}
+
+function copypassword(){
+    PasswordBox.select();
+    document.execCommand("copy");
+
+}
